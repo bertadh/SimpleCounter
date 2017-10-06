a1ff57c2078dc33e858dfc4b0920075b2e8098a4
bertadh <bertadomingo1@hotmail.com>
2017-10-06 15:48:01 +0200

    Versio inicial

---
+++ app/src/main/java/edu/upc/eseiaat/pma/simplecounter/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.upc.eseiaat.pma.simplecounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
