dd34654502128dd73b077c0fec62b6e35f34d4c8
bertadh <bertadomingo1@hotmail.com>
2017-10-06 16:22:23 +0200

    Versio 1.0

--- app/src/main/java/edu/upc/eseiaat/pma/simplecounter/MainActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/simplecounter/MainActivity.java
@@ -2,13 +2,33 @@ package edu.upc.eseiaat.pma.simplecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edit_num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit_num = (EditText) findViewById(R.id.edit_num);
+    }
+
+    public void increase (View v){
+        Log.i("bertadh","Increment clicat");
+
+        String snum = edit_num.getText().toString();
+        int num = Integer.parseInt(snum)+1;
+        snum = Integer.toString(num);
+        edit_num.setText(snum);
+    }
+
+    public void decrement(View v){
+        //TODO: Decrementar el numero
+        Log.i("bertadh","decrement clicat");
+        //edit_num = edit_num.getText()-1;
     }
 }
